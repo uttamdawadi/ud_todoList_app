b428cdb700c0ab3ecc863137efdea7fc60a1f576
Uttam Dawadi <shipnowapp@gmail.com>
2022-04-04 00:20:54 +1000

    Update with: -missing files

---
+++ tests/Unit/Sample.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Sample extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
